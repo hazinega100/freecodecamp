0fef7691981ad3ea488cbfa024f2600ea8420b0a
Egor <hazinega@gmail.com>
2023-01-09 18:34:57 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
+"use strict";
+
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
